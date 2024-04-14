9ee3aec0457524bbaad5f9fabf9ba52b6e8adbdb
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2024-04-13 19:44:40 -0600

    Se cambió el logo en el dashboard de la aplicación

--- app/View/Components/AppBrand.php
+++ app/View/Components/AppBrand.php
@@ -22,22 +22,18 @@ class AppBrand extends Component
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                <a href="/" wire:navigate>
-                    <!-- Hidden when collapsed -->
-                    <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
-                        <div class="flex items-center gap-2">
-                            <x-icon name="o-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
-                            <span class="font-bold text-3xl mr-3 bg-gradient-to-r from-purple-500 to-pink-300 bg-clip-text text-transparent ">
-                                app
-                            </span>
-                        </div>
-                    </div>
+        <a href="/" wire:navigate>
 
-                    <!-- Display when collapsed -->
-                    <div class="display-when-collapsed hidden mx-5 mt-4 lg:mb-6 h-[28px]">
-                        <x-icon name="s-square-3-stack-3d" class="w-6 -mb-1 text-purple-500" />
-                    </div>
-                </a>
-            HTML;
+            <!-- Hidden when collapsed -->
+            <div {{ $attributes->class(["hidden-when-collapsed"]) }}>
+                <img src="{{ Vite::asset('resources/img/logo_full.png') }}" alt="Logo" class="h-10" />
+            </div>
+
+            <!-- Display when collapsed -->
+            <div class="display-when-collapsed mt-4 hidden lg:mb-6">
+                <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="Logo" class="mx-auto h-8" />
+            </div>
+        </a>
+        HTML;
     }
 }

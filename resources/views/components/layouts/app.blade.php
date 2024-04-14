89ec33f5459a0edca8757d166397f2502fc8f776
Alonso Ballesteros Torres <Foxuper@hotmail.com>
2024-04-13 20:00:53 -0600

    Se limpiaron las vistas de layouts

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
@@ -8,7 +9,8 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="min-h-screen font-sans antialiased bg-base-200/50 dark:bg-base-200">
+
+<body class="min-h-screen bg-base-200/50 font-sans antialiased dark:bg-base-200">
 
     {{-- NAVBAR mobile only --}}
     <x-nav sticky class="lg:hidden">
@@ -16,14 +18,15 @@
             <x-app-brand />
         </x-slot:brand>
         <x-slot:actions>
-            <label for="main-drawer" class="lg:hidden mr-3">
+            <label for="main-drawer" class="mr-3 lg:hidden">
                 <x-icon name="o-bars-3" class="cursor-pointer" />
             </label>
         </x-slot:actions>
     </x-nav>
 
     {{-- MAIN --}}
-    <x-main full-width>
+    <x-main full-width :collapse-text="__('Collapse')">
+
         {{-- SIDEBAR --}}
         <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-100 lg:bg-inherit">
 
@@ -35,22 +38,17 @@
 
                 {{-- User --}}
                 @if($user = auth()->user())
-                    <x-menu-separator />
-
-                    <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="-mx-2 !-my-2 rounded">
-                        <x-slot:actions>
-                            <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" tooltip-left="logoff" no-wire-navigate link="/logout" />
-                        </x-slot:actions>
-                    </x-list-item>
-
-                    <x-menu-separator />
+                <x-menu-separator />
+                <x-list-item :item="$user" value="name" sub-value="email" no-separator no-hover class="!-my-2 -mx-2 rounded">
+                    <x-slot:actions>
+                        <x-button icon="o-power" class="btn-circle btn-ghost btn-xs" :tooltip-left="__('Logout')" no-wire-navigate link="/logout" />
+                    </x-slot:actions>
+                </x-list-item>
+                <x-menu-separator />
                 @endif
 
-                <x-menu-item title="Hello" icon="o-sparkles" link="/" />
-                <x-menu-sub title="Settings" icon="o-cog-6-tooth">
-                    <x-menu-item title="Wifi" icon="o-wifi" link="####" />
-                    <x-menu-item title="Archives" icon="o-archive-box" link="####" />
-                </x-menu-sub>
+                {{-- Menu items --}}
+                <x-menu-item :title="__('Home')" icon="o-home" link="/" />
             </x-menu>
         </x-slot:sidebar>
 
@@ -60,7 +58,8 @@
         </x-slot:content>
     </x-main>
 
-    {{--  TOAST area --}}
+    {{-- TOAST area --}}
     <x-toast />
 </body>
+
 </html>
